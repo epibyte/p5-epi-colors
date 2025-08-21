0d1eeadc67acbbc65ea82daba528da55752b692f
epibyte <boettcher.f@googlemail.com>
2025-08-21 11:12:17 +0200

    corrected jsdelvr path

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 // run via: npx rollup -c
 // git tag v0.0.1        
 // git push origin v0.0.1
-// call: https://cdn.jsdelivr.net/gh/epibyte/p5-epi-shapesvg@v0.0.1/dist/p5-epi-colors.js
+// call: https://cdn.jsdelivr.net/gh/epibyte/p5-epi-colors@v0.0.1/dist/p5-epi-colors.js
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
